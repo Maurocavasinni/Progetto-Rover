f74db4a72310d6307834df2479b6e73825d30c82
Maurocavasinni <cavasinnimauro@gmail.com>
2025-12-17 11:05:00 +0100

    Introdotti loop principale e salvataggio log

--- ProgettoRover.py
+++ ProgettoRover.py
@@ -3,6 +3,7 @@
 
 import RPi.GPIO as GPIO
 import time
+import logging
 
 SAFE_DISTANCE = 0.30
 DANGER_DISTANCE = 0.15
@@ -26,6 +27,12 @@ ECHO = 4
 LED0 = 10
 LED1 = 9
 LED2 = 25
+
+logging.basicConfig(
+    filename='rover_patrol.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 def setup_gpio():
     # Motor setup
@@ -90,6 +97,9 @@ def motor_stop():
 
 def piroettonj():
     motor_turn_left()
+    motor_turn_left()
+    motor_turn_left()
+    motor_turn_left()
 
     time.sleep(5)
 
@@ -106,3 +116,58 @@ def collision_avoidance():
         motor_stop()
     else:
         motor_forward()
+
+def get_distance():
+    GPIO.output(TRIG, GPIO.HIGH)
+    time.sleep(0.000015)
+    GPIO.output(TRIG, GPIO.LOW)
+    
+    while not GPIO.input(ECHO):
+        pass
+    t1 = time.time()
+    
+    while GPIO.input(ECHO):
+        pass
+    t2 = time.time()
+    
+    distance = (t2 - t1) * 340 / 2
+    return distance
+
+def loop_rover():
+    while True:
+        motor_turn_left()
+        distance_left = get_distance()
+        logging.info(f"Distanza sinistra: {distance_left:.2f}m")
+        motor_turn_right()
+        distance_center = get_distance()
+        logging.info(f"Distanza centro: {distance_center:.2f}m")
+        motor_turn_right()
+        distance_right = get_distance()
+        logging.info(f"Distanza destra: {distance_right:.2f}m")
+        motor_turn_left()
+
+        #TODO: Fix if
+        if (distance_center > distance_left):
+            if (distance_center > distance_right):
+                motor_forward()
+                time.sleep(1)
+            else:
+                motor_turn_right()
+                motor_forward()
+                time.sleep(1)
+        else:
+            motor_turn_left()
+            motor_forward()
+            time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        setup_gpio()
+        loop_rover()
+        
+    except KeyboardInterrupt:
+        motor_stop()
+        GPIO.output(LED0, GPIO.HIGH)
+        GPIO.output(LED1, GPIO.HIGH)
+        GPIO.output(LED2, GPIO.HIGH)
+        GPIO.cleanup()
